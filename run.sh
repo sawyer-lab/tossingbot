da825fc0c1166b62b7e059f5456f63d89d668bd4
fausto <gomezcorderojf@gmail.com>
2025-05-18 19:10:07 -0600

    Update run.sh and Dockerfile for NVIDIA setup

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
-xhost +
+#!/bin/bash
+
+xhost +local:root
+
 docker run -d --rm \
-	--network host \
-	-e DISPLAY=$DISPLAY \
-	-v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-	--mount type=bind,source=./ros_src/cinves_perception,target=/cinves_perception \
-	--name robo2025 \
-	-it \
-	--privileged \
-	robo2025-workspace:latest "$@"
-
-
-# --device=/dev/bus/usb/003/002 \
+    --runtime=nvidia \
+    --gpus all \
+    --network host \
+    -e DISPLAY=$DISPLAY \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e __NV_PRIME_RENDER_OFFLOAD=1 \
+    -e __GLX_VENDOR_LIBRARY_NAME=nvidia \
+    -e __VK_LAYER_NV_optimus=NVIDIA_only \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --mount type=bind,source=./ros_src/cinves_perception,target=/cinves_perception \
+    --name robo2025 \
+    -it \
+    --privileged \
+    robo2025-workspace:latest "$@"
